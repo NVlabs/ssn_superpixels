8888ab39d782561a6b0f613452fe8a84b1c56311
Varun Jampani <varunjampani@users.noreply.github.com>
2018-10-05 11:12:51 -0400

    Add sintel trained SSN model

--- models/get_models.sh
+++ models/get_models.sh
@@ -3,3 +3,4 @@
 curl -L https://www.dropbox.com/s/kjugte52o6pqhsk/ssn_bsds_model.caffemodel?dl=0 > ssn_bsds_model.caffemodel
 curl -L https://www.dropbox.com/s/g12e0e71z5bclf9/ssn_cityscapes_model.caffemodel?dl=0 > ssn_cityscapes_model.caffemodel
 curl -L https://www.dropbox.com/s/f7r15b7yom5f57o/ssn_voc_model.caffemodel?dl=0 > ssn_voc_model.caffemodel
+curl -L https://www.dropbox.com/s/qbck66abf7ek9gv/ssn_sintel_model.caffemodel?dl=0 > ssn_sintel_model.caffemodel
